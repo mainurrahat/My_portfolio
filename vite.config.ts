bb9b01abaa834f684881e8932920ac84745ac310
mainurrahat <mainurrahat51@gmail.com>
2025-09-15 12:19:36 +0600

    project chara baki gula updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
